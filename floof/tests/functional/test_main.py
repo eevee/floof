b02d51ced26265f469b9d01d313633b1dac38fa7
epii <epii@leptic.net>
2011-02-24 11:11:42 +0000

    Add a rudimentary fuctional test for the "Cookies Disabled" error page.

--- floof/tests/functional/test_main.py
+++ floof/tests/functional/test_main.py
@@ -3,12 +3,16 @@ from floof.tests import *
 class TestMainController(TestController):
 
     def test_index(self):
-        """Test that the front page loads."""
+        """Test display of the front page."""
         response = self.app.get(url(controller='main', action='index'))
         # Test response...
 
     def test_log(self):
-        """Test that the public admin log page loads."""
+        """Test display of the public admin log page."""
         response = self.app.get(url(controller='main', action='log'))
         assert 'Public Admin Log' in response
 
+    def test_cookies_disabled(self):
+        """Test display of the "cookies disabled" error page."""
+        response = self.app.get(url(controller='main', action='cookies_disabled'))
+        assert 'Cookies Disabled' in response
