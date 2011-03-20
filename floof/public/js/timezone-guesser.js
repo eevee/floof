77c0522fc47fae8709ee240d6cb691ad308bc1df
epii <epii@leptic.net>
2011-03-20 02:13:27 +0000

    Add a couple of comments to the JS timezone guesser.  #471

--- floof/public/js/timezone-guesser.js
+++ floof/public/js/timezone-guesser.js
@@ -1,13 +1,15 @@
+// On page load, set the timezone to our best guess
 $(document).ready(function() {
     var tzs = document.getElementById("timezone");
+    // Only change it if it's at its default of UTC
     if (tzs.options[tzs.selectedIndex].value == "UTC") {
         var tz = determine_timezone().timezone.olson_tz;
-        if (tz != "UTC") {
-            for (var i = 0; i < tzs.options.length; i++) {
-                if (tzs.options[i].value == tz) {
-                    tzs.options.selectedIndex = i;
-                    break;
-                }
+        // tz's Olson name may not be in our list, so this may silently fail
+        // under some circumstances, but that should be okay.
+        for (var i = 0; i < tzs.options.length; i++) {
+            if (tzs.options[i].value == tz) {
+                tzs.options.selectedIndex = i;
+                break;
             }
         }
     }
