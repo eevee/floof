aac819d0745dfd5bd4f2493a9c6bdb8fe6836dd1
maggymay <maggymay@maggymay-VirtualBox.(none)>
2012-04-03 09:54:19 -0400

    Git told me to do it :( Nothing's changed. Played with different request methods though!

--- floof/views/api.py
+++ floof/views/api.py
@@ -7,6 +7,8 @@ from floof import model
 
 log = logging.getLogger(__name__)
 
+# TODO Now, the fun: return format determined by file extension or accepted headers
+# Methods executed by extraneous URIs "/api/delete/:id" or by request method GET, DELETE, etc.
 @view_config(
         route_name='api.test',
         request_method='GET',
