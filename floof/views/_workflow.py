173d7d762fdc068e7c1d0e59b0da2f76e9f305ba
epii <epii@leptic.net>
2012-08-19 00:38:21 +1000

    Add helpers for simple CRUD workflow views.
    TODO: properly abstract business logic from display method so we can re-use
    CRUDWorkflow subclasses in a JSON API.

--- floof/views/_workflow.py
+++ floof/views/_workflow.py
@@ -1,6 +1,11 @@
 """Helper classes for implementing common workflows."""
+
+import wtforms
+
 from pyramid.renderers import render_to_response
 from pyramid.httpexceptions import HTTPSeeOther
+
+from floof import model
 
 
 class FormWorkflow(object):
@@ -61,7 +66,7 @@ class FormWorkflow(object):
             return self.respond_json(ret)
         else:
             return self.show_form()
-        
+
     def respond_redirect(self, url):
         if self.request.is_xhr:
             # NOTE: do NOT use _new_json_response here; it eats the flash!
@@ -71,5 +76,146 @@ class FormWorkflow(object):
             return self.respond_json(ret)
         else:
             return HTTPSeeOther(location=url)
-        
 
+
+class CRUDWorkflow(FormWorkflow):
+    """Workflows for manipulating (single) ORM objects.
+
+    GETs just display the form defined as :attr:`form_class`.
+
+    POSTs perform the creation, updation, deletion, etc and then redirect (303)
+    the user to the :attr:`redirect_location` or :attr:`redirect_route`.
+
+    """
+    # TODO: Make this work meaningfully with FormWorkflow AJAXisms
+    # TODO: Properly abstract the business logic for easy integration into both
+    # web view and a future (imminent?) API.
+
+    # CHANGEME: Where to redirect after success.  redirect_location may be a
+    # string that will be passed directly as the Location HTTP header or a
+    # callable accepting the workflow object and returning such a string;
+    # alternatively, redirect_route may be used as a shortcut to specify a
+    # plain route name to be passed to request.route_url
+    redirect_location = None
+    redirect_route = 'root'
+
+    # CHANGEME: Name to use to reference the ORM obj in the "successfully
+    # created" flash message; may be a callable that accepts the workflow obj
+    # Omit to suppress the flash message
+    flash_name = None
+
+    def _get_redirect(self):
+        if self.redirect_location is not None:
+            if hasattr(self.redirect_location, '__call__'):
+                location = self.redirect_location()
+            else:
+                location = self.redirect_location
+        else:
+            location = self.request.route_url(self.redirect_route)
+
+        return HTTPSeeOther(location=location)
+
+    def handle_get(self):
+        return self.show_form()
+
+    def add_flash(self, obj, msg, **kwargs):
+        callable_ = hasattr(self.flash_name, '__call__')
+        name = self.flash_name(obj) if callable_ else self.flash_name
+
+        if name:
+            msg = msg.format(name=name)
+            self.request.session.flash(msg, **kwargs)
+
+
+class CreateWorkflow(CRUDWorkflow):
+    """Workflow for creating a single ORM object."""
+
+    # CHANGEME: ORM class of which an instance shall be created
+    orm_cls = None
+
+    def extra_attrs(self):
+        return dict()
+
+    def handle_post(self):
+        if not self.form.validate():
+            return self.respond_form_error()
+
+        # Prepare to populate a new object with appropriate form fields
+        orm_attrs = {}
+        for field in self.form:
+            if hasattr(self.orm_cls, field.short_name):
+                orm_attrs[field.short_name] = field.data
+
+        orm_attrs.update(self.extra_attrs())
+
+        self.newobj = self.orm_cls(**orm_attrs)
+        model.session.add(self.newobj)
+        model.session.flush()  # Ensure the new obj exists before rdr
+
+        self.add_flash(
+            self.newobj, u'{name} successfully created.', level=u'success',
+            icon='plus')
+
+        return self._get_redirect()
+
+
+class UpdateWorkflow(CRUDWorkflow):
+    """Workflow for editing a single ORM object.
+
+    The ORM object must be supplied as the request's context.
+
+    """
+    # CHANGEME: Name under which to make the ORM obj available in the template
+    context_name = 'changeme'
+
+    def make_form(self):
+        return self.form_class(self.request.POST, obj=self.request.context)
+
+    def show_form(self):
+        return {'form': self.form, self.context_name: self.request.context}
+
+    def tweak_form(self):
+        pass
+
+    def handle_post(self):
+        if not self.form.validate():
+            return self.respond_form_error()
+
+        self.tweak_form()
+
+        self.form.populate_obj(self.request.context)
+
+        self.add_flash(
+            self.request.context, u'{name} successfully updated.',
+            level=u'success', icon='pencil')
+
+        return self._get_redirect()
+
+
+class DeleteWorkflow(CRUDWorkflow):
+    """Workflow for editing a single ORM object.
+
+    The ORM object must be supplied as the request's context.
+
+    """
+    # CHANGEME: Name under which to make the ORM obj available in the template
+    context_name = 'changeme'
+
+    class form_class(wtforms.form.Form):
+        delete = wtforms.fields.SubmitField(u'Delete')
+
+    def show_form(self):
+        return {'form': self.form, self.context_name: self.request.context}
+
+    def handle_post(self):
+        if not self.form.validate():
+            return self.respond_form_error()
+
+        model.session.delete(self.request.context)
+        model.session.flush()
+
+        self.add_flash(
+            self.request.context, u'{name} successfully deleted.',
+            level=u'success', icon='minus')
+
+        return self._get_redirect()
