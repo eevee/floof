e1eac4f07a3d5d40f1d8214214d02a7ce251a5f1
epii <epii@leptic.net>
2012-09-13 06:49:58 +0000

    Automatically stamp new DB with alembic version.
    When initialized via bin/setup-floof.py.

--- bin/setup-floof.py
+++ bin/setup-floof.py
@@ -4,6 +4,8 @@ import os
 import transaction
 import sys
 
+from alembic import command
+from alembic.config import Config
 from paste.deploy import appconfig
 from sqlalchemy import engine_from_config
 from zope.sqlalchemy import ZopeTransactionExtension
@@ -15,12 +17,14 @@ from floof import model
 log = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2 or sys.argv[1] in ('-h', '--help'):
-        print 'usage: python {0} config-file.ini#app-name'.format(sys.argv[0])
+    if len(sys.argv) < 3 or sys.argv[1] in ('-h', '--help'):
+        print 'usage: python {0} config-file.ini#app-name alembic.ini'.format(sys.argv[0])
         sys.exit(0)
 
-    # Get paster to interpret the passed config file
     ini_spec = os.path.abspath(sys.argv[1])
+    alembic_ini_path = os.path.abspath(sys.argv[2])
+
+    # Get paster to interpret the passed config file
     conf = appconfig('config:' + ini_spec)
 
     # Set up the SQLAlchemy environment
@@ -29,6 +33,8 @@ if __name__ == '__main__':
         extension=ZopeTransactionExtension())
 
     populate_db(model.TableBase.metadata, model.session)
+    alembic_cfg = Config(alembic_ini_path)
+    command.stamp(alembic_cfg, 'head')
     generate_ca(conf)
 
     # XXX: This may be bad juju
